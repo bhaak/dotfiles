928bc83cfb687fa3842940f34b87ae0226c42fbc
Patric Mueller <bhaak@gmx.net>
2019-06-10 17:00:45 +0200

    First install script version

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+echo "Linking files..."
+BASEDIR="$(cd "$(dirname $0)" && pwd)"
+
+function link()
+{
+	SOURCE=$1
+	DEST=$2
+	error="\e[31m"
+	okay="\e[32m"
+
+	echo -n "Linking '$SOURCE' to '$HOME/$DEST' ... "
+	if [[ -f "$HOME/$DEST" ]]; then
+		echo -e "$error" "exists already"
+	else
+		ln "$BASEDIR/$SOURCE" "$HOME/$DEST"
+		echo -e "$okay" "linked"
+	fi
+}
