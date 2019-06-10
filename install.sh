85f528f0257f1153e93186c73606512c5edf91a7
Patric Mueller <bhaak@gmx.net>
2019-06-10 17:28:05 +0200

    Add automatic directiony creation to installation script

--- install.sh
+++ install.sh
@@ -7,15 +7,21 @@ function link()
 {
 	SOURCE=$1
 	DEST=$2
+	DESTDIR=$(dirname "$DEST")
 	error="\e[31m"
 	okay="\e[32m"
+	reset="\e[0m"
+
+	if [[ ! -d "$HOME/$DESTDIR" ]]; then
+		mkdir -p "$HOME/$DESTDIR"
+	fi
 
 	echo -n "Linking '$SOURCE' to '$HOME/$DEST' ... "
 	if [[ -f "$HOME/$DEST" ]]; then
-		echo -e "$error" "exists already"
+		echo -e "$error" "exists already" "$reset"
 	else
 		ln "$BASEDIR/$SOURCE" "$HOME/$DEST"
-		echo -e "$okay" "linked"
+		echo -e "$okay" "linked" "$reset"
 	fi
 }
 
